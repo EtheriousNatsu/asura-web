611607dc39fd719893be11c856350fe263adb0dc
周强 <zhouqiang@zhouqiangdeMacBook-Pro-2.local>
2021-08-19 16:38:49 +0800

    调整asgi

--- asura/asgi.py
+++ asura/asgi.py
@@ -5,12 +5,11 @@
 @file: asgi.py	
 @time: 2021/8/18	
 """
+
 import os
-import configurations
+import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asura.config")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
-configurations.setup()
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asura.config.production')
+django.setup()
 application = get_default_application()
